5b5483bcb2960f237a727b6ebd6f18c3e9d93bce
oleg <oleg056@gmail.com>
2013-03-01 21:58:24 +0200

    56567

---
+++ asd.js
@@ -0,0 +1,10 @@
+/**
+ * Created with JetBrains WebStorm.
+ * User: Oleg
+ * Date: 01.03.13
+ * Time: 21:35
+ * To change this template use File | Settings | File Templates.
+ */
+(function(){
+
+})();
